ad3e6212dc58dbc8811788518c3f51fc005dbba8
Jacob Leonardo <leonardojacob257@gmail.com>
2023-11-17 15:30:20 -0700

    Improved Player Movement Script

--- Jacob/PlayerController.cs
+++ Jacob/PlayerController.cs
@@ -4,9 +4,8 @@ using System;
 
 public partial class PlayerController : CharacterBody2D
 {
-	private float speed = 500.0f;
+	[Export] public float speed = 500.0f;
 
-	private Vector2 velocity = Vector2.Zero;
 	private Vector2 input = Vector2.Zero;
 
 
@@ -18,52 +17,22 @@ public partial class PlayerController : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		ProcessInput();
-		MovePlayer(delta);
-		LookAtMouse();
+		
 	}
 
+    public override void _PhysicsProcess(double delta)
+    {
+		// Rotates player to look at the mouse position
+		LookAt(GetGlobalMousePosition());
+		ProcessInput();
+		Velocity = input * speed;
+		MoveAndSlide();
+    }
 	private void ProcessInput()
 	{
-		input = new Vector2();
-
-		if (Input.IsActionPressed("up"))
-		{
-			input.Y -= 1;
-		}
-		if (Input.IsActionPressed("down"))
-		{
-			input.Y += 1;
-		}
-		if (Input.IsActionPressed("left"))
-		{
-			input.X -= 1;
-		}
-		if (Input.IsActionPressed("right"))
-		{
-			input.X += 1;
-		}
+		input = Input.GetVector("left", "right", "up", "down");
 
 		// Normalize input vector to prevent faster diagonal movement
 		input = input.Normalized();
-	}
-
-	private void MovePlayer(double delta)
-	{
-		// Set velocity based on input and speed
-		velocity = input * speed;
-		// Update the player's position
-		Position += velocity * (float)delta;
-
-	}
-
-	private void LookAtMouse()
-	{
-		Vector2 mousePos = GetGlobalMousePosition();
-
-		// Angle between player character and mouse
-		float angle = Mathf.Atan2(mousePos.Y - this.GlobalPosition.Y, mousePos.X - this.GlobalPosition.X);
-
-		this.RotationDegrees = Mathf.RadToDeg(angle);
 	}
 }
