fe8d7ac5c43ec5d94f01e1b3fd01ddf7a6e0fc85
PipoPleading <129329931+PipoPleading@users.noreply.github.com>
2023-11-17 19:40:43 -0700

    Update cursorTest.gd

--- Piper/cursorTest.gd
+++ Piper/cursorTest.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	$Track1.volume_db = -80;
 	$Track2.volume_db = -80;
 	pass # Replace with function body.
