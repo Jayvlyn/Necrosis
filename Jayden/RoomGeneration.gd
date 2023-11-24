dd572704657f35bf199be1bcda408dad35a94d40
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2023-11-24 15:13:59 -0700

    more progress
    not done im almost there

--- Jayden/RoomGeneration.gd
+++ Jayden/RoomGeneration.gd
@@ -1,4 +1,8 @@
-extends TileMap
+extends Node2D
+
+var currentScene
+
+var numRoom = 5
 
 var width = 10
 var height = 10
@@ -12,5 +16,17 @@ func _process(delta):
 	pass
 
 func generate_layout():
-	var tile_pos = local_to_map(position) #converts position passed into tilemap coords
-	
+	#var tile_pos = local_to_map(position) #converts position passed into tilemap coords
+	#randi_range(1, 1)
+	var scene = load("res://Jayden/Rooms/room_1.tscn")
+	var instance = scene.instantiate()
+	add_child(instance)
+
+
+
+
+#func add_my_node():
+
+
+ #set position if needed
+ #myNode_instance.global_transform = global_transform
