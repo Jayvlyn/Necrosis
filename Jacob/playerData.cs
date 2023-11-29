74a7dc69a877b12a97ae08125d8f2ec78d3f4fd3
Jacob Leonardo <leonardojacob257@gmail.com>
2023-11-28 19:26:36 -0700

    Leveling added to playerData.cs

--- Jacob/playerData.cs
+++ Jacob/playerData.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class playerData : Node
 {
+    // Player class
     public enum Class
     {
         Tanker,
@@ -11,13 +12,43 @@ public partial class playerData : Node
     }
 
     [Export] public Class playerClass;
+
+
+    // Player attributes
     [Export] public float moveSpeed = 600.0f;
-    [Export] public int maxMass = 100;
+    [Export] public uint maxMass = 100;
     [Export] public float bulletSpeed = 600.0f;
     [Export] public float bulletsPerSecond = 5.0f;
     [Export] public float bulletDamage = 30.0f;
-    [Export] public int massPerBullet = 5;
+    [Export] public uint massPerBullet = 5;
+    
 
+    // Stats
     public int kills = 0;
-    public int experience = 0;
+
+
+    // Leveling
+    [Export] public int levelScaling = 2; // how large the expNeeded increases per level
+    [Export] public int level1Cost = 100;
+    public int level = 1;
+    public uint experience = 0;
+
+    public void GainExp(uint amount)
+    {
+        double expNeeded = level1Cost * Math.Pow(level, levelScaling); // multiplies a base level cost by an exponentially growing level scaling
+        if (experience + amount > expNeeded)
+        { 
+            LevelUp();
+        }
+        else
+        {
+            experience += amount;
+        }
+    }
+
+    public void LevelUp()
+    {
+        experience = 0; // Reset experience on level up
+        level++;
+    }
 }
