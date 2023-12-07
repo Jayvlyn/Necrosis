ad45b47c5f0957ad2957d5511b1392262d6788c0
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2023-12-06 19:08:06 -0700

    final room tiles and room area2D reduction

--- Jayden/Rooms/finalRoomLogic.gd
+++ Jayden/Rooms/finalRoomLogic.gd
@@ -18,7 +18,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	processInput()
 
 func _on_room_area_body_entered(body):
 	if (body.is_in_group("Player") && !roomGenerated):
@@ -28,6 +28,13 @@ func _on_room_area_body_entered(body):
 		for n in rng.randi_range(3, 5):
 			enemySpawn()
 		roomGenerated = true
+
+func processInput():
+	if Input.is_action_just_pressed("debug"):
+		enemies -= 1
+		print("enemies: " + str(enemies))
+	if (enemies <= 0 && roomGenerated):
+		roomComplete()
 
 func _on_room_area_body_exited(body):
 	if (body.is_in_group("Enemy") && body.dead == true && roomGenerated):
@@ -39,10 +46,10 @@ func _on_room_area_body_exited(body):
 func roomSetup():
 	await get_tree().create_timer(0.5).timeout
 	for n in 4:
-			map.set_cell(0, Vector2i(0, 7 + n), 0, Vector2i(0, 1)) #left wall
-			map.set_cell(0, Vector2i(17, 7 + n), 0, Vector2i(2, 1)) #right wall
-			map.set_cell(0, Vector2i(7 + n, 0), 0, Vector2i(1, 0)) #top wall
-			map.set_cell(0, Vector2i(7 + n, 17), 0, Vector2i(1, 2)) #bottom wall
+			map.set_cell(0, Vector2i(0, 7 + n), 1, Vector2i(0, 1)) #left wall
+			map.set_cell(0, Vector2i(17, 7 + n), 1, Vector2i(2, 1)) #right wall
+			map.set_cell(0, Vector2i(7 + n, 0), 1, Vector2i(1, 0)) #top wall
+			map.set_cell(0, Vector2i(7 + n, 17), 1, Vector2i(1, 2)) #bottom wall
 
 func enemySpawn():
 	var roomOrigin = map.local_to_map(to_local(roomPos.position))
@@ -67,22 +74,22 @@ func enemySpawn():
 func roomComplete():
 	var roomOrigin = map.local_to_map(to_local(roomPos.position))
 	
-	map.set_cell(0, Vector2i(0, 0 + 7), 0, Vector2i(3, 1)) #left tiles
-	map.set_cell(0, Vector2i(0, 0 + 8), 0, Vector2i(1, 1))
-	map.set_cell(0, Vector2i(0, 0 + 9), 0, Vector2i(1, 1))
-	map.set_cell(0, Vector2i(0, 0 + 10), 0, Vector2i(3, 2))
+	map.set_cell(0, Vector2i(0, 0 + 7), 1, Vector2i(4, 1)) #left tiles
+	map.set_cell(0, Vector2i(0, 0 + 8), 1, Vector2i(1, 1))
+	map.set_cell(0, Vector2i(0, 0 + 9), 1, Vector2i(1, 1))
+	map.set_cell(0, Vector2i(0, 0 + 10), 1, Vector2i(4, 1))
 	
-	map.set_cell(0, Vector2i(0 + 17, 0 + 7), 0, Vector2i(3, 1)) #right tiles
-	map.set_cell(0, Vector2i(0 + 17, 0 + 8), 0, Vector2i(1, 1))
-	map.set_cell(0, Vector2i(0 + 17, 0 + 9), 0, Vector2i(1, 1))
-	map.set_cell(0, Vector2i(0 + 17, 0 + 10), 0, Vector2i(3, 2))
+	map.set_cell(0, Vector2i(0 + 17, 0 + 7), 1, Vector2i(3, 1)) #right tiles
+	map.set_cell(0, Vector2i(0 + 17, 0 + 8), 1, Vector2i(1, 1))
+	map.set_cell(0, Vector2i(0 + 17, 0 + 9), 1, Vector2i(1, 1))
+	map.set_cell(0, Vector2i(0 + 17, 0 + 10), 1, Vector2i(3, 0))
 	
-	map.set_cell(0, Vector2i(0 + 7, 0), 0, Vector2i(2, 3)) #top tiles
-	map.set_cell(0, Vector2i(0 + 8, 0), 0, Vector2i(1, 1))
-	map.set_cell(0, Vector2i(0 + 9, 0), 0, Vector2i(1, 1))
-	map.set_cell(0, Vector2i(0 + 10,0), 0, Vector2i(0, 3))
+	map.set_cell(0, Vector2i(0 + 7, 0), 1, Vector2i(3, 3)) #top tiles
+	map.set_cell(0, Vector2i(0 + 8, 0), 1, Vector2i(1, 1))
+	map.set_cell(0, Vector2i(0 + 9, 0), 1, Vector2i(1, 1))
+	map.set_cell(0, Vector2i(0 + 10,0), 1, Vector2i(2, 3))
 	
-	map.set_cell(0, Vector2i(0 + 7, 0 + 17), 0, Vector2i(2, 3)) #bottom tiles
-	map.set_cell(0, Vector2i(0 + 8, 0 + 17), 0, Vector2i(1, 1))
-	map.set_cell(0, Vector2i(0 + 9, 0 + 17), 0, Vector2i(1, 1))
-	map.set_cell(0, Vector2i(0 + 10, 0 + 17), 0, Vector2i(0, 3))
+	map.set_cell(0, Vector2i(0 + 7, 0 + 17), 1, Vector2i(2, 3)) #bottom tiles
+	map.set_cell(0, Vector2i(0 + 8, 0 + 17), 1, Vector2i(1, 1))
+	map.set_cell(0, Vector2i(0 + 9, 0 + 17), 1, Vector2i(1, 1))
+	map.set_cell(0, Vector2i(0 + 10, 0 + 17), 1, Vector2i(0, 3))
